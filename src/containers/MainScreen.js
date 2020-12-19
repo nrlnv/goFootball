d23cc83a3ba183a39359cd52be340c1d1b160d72
David Luiz <kkarzhauov@hrsinternational.com>
2020-12-19 16:45:17 +0500

    added vector icons

--- src/containers/MainScreen.js
+++ src/containers/MainScreen.js
@@ -1,12 +1,19 @@
 import React from 'react';
-import {SafeAreaView, Text} from 'react-native';
+import {SafeAreaView, Text, StyleSheet} from 'react-native';
 
 const MainScreen = () => {
   return (
-    <SafeAreaView>
+    <SafeAreaView style={styles.container}>
       <Text>Main screen</Text>
     </SafeAreaView>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: 'white',
+  },
+});
 
 export default MainScreen;
--- src/routers/MainTabs.js
+++ src/routers/MainTabs.js
@@ -3,6 +3,9 @@ import React from 'react';
 import MainScreen from '../containers/MainScreen';
 import ProfileScreen from '../containers/ProfileScreen';
 
+import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
+import MaterialIcons from 'react-native-vector-icons/MaterialIcons';
+
 import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
 
 const Tab = createBottomTabNavigator();
@@ -10,8 +13,30 @@ const Tab = createBottomTabNavigator();
 const MainTabs = () => {
   return (
     <Tab.Navigator initialRouteName="MainScreen">
-      <Tab.Screen name="MainScreen" component={MainScreen} />
-      <Tab.Screen name="ProfileScreen" component={ProfileScreen} />
+      <Tab.Screen
+        name="MainScreen"
+        component={MainScreen}
+        options={{
+          tabBarLabel: 'Games',
+          tabBarIcon: ({color}) => (
+            <MaterialCommunityIcons
+              name="soccer-field"
+              color={color}
+              size={24}
+            />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="ProfileScreen"
+        component={ProfileScreen}
+        options={{
+          tabBarLabel: 'Profile settings',
+          tabBarIcon: ({color}) => (
+            <MaterialIcons name="person-outline" color={color} size={24} />
+          ),
+        }}
+      />
     </Tab.Navigator>
   );
 };
