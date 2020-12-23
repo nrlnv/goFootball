f4951f2a6e74d38f25beda116ac869a416c8d460
David Luiz <kkarzhauov@hrsinternational.com>
2020-12-23 13:36:12 +0500

    removed photo picker

--- src/containers/ProfileScreen.js
+++ src/containers/ProfileScreen.js
@@ -14,19 +14,17 @@ const ProfileScreen = ({navigation}) => {
   const [photo, setPhoto] = useState(null);
   const [photoUrl, setPhotoUrl] = useState(null);
 
-  var storageRef = firebase.storage().ref();
-
-  const handleChoosePhoto = () => {
-    const options = {
-      noData: true,
-    };
-    ImagePicker.launchImageLibrary(options, (response) => {
-      if (response.uri) {
-        setPhoto(response);
-        console.log('image: ', response);
-      }
-    });
-  };
+  // const handleChoosePhoto = () => {
+  //   const options = {
+  //     noData: true,
+  //   };
+  //   ImagePicker.launchImageLibrary(options, (response) => {
+  //     if (response.uri) {
+  //       setPhoto(response);
+  //       console.log('image: ', response);
+  //     }
+  //   });
+  // };
 
   var user = firebase.auth().currentUser;
 
@@ -36,10 +34,10 @@ const ProfileScreen = ({navigation}) => {
         setEmail(authenticate.email);
         setName(authenticate.displayName);
         setPhotoUrl(authenticate.photoURL);
-        console.log(
-          'user photo: ',
-          authenticate.photoURL.slice(0, photoUrl.lastIndexOf('?')),
-        );
+        // console.log(
+        //   'user photo: ',
+        //   authenticate.photoURL.slice(0, photoUrl.lastIndexOf('?')),
+        // );
       } else {
         navigation.replace('SignInScreen');
       }
@@ -58,69 +56,69 @@ const ProfileScreen = ({navigation}) => {
       });
   };
 
-  const saveSettings = () => {
-    // Create the file metadata
-    var metadata = {
-      contentType: 'image/jpeg',
-    };
+  // const saveSettings = () => {
+  //   // Create the file metadata
+  //   var metadata = {
+  //     contentType: 'image/jpeg',
+  //   };
 
-    // Upload file and metadata to the object 'images/mountains.jpg'
-    var uploadTask = storageRef
-      .child('images/' + photo.fileName)
-      .put(photo, metadata);
+  //   // Upload file and metadata to the object 'images/mountains.jpg'
+  //   var uploadTask = storageRef
+  //     .child('images/' + photo.fileName)
+  //     .put(photo, metadata);
 
-    // Listen for state changes, errors, and completion of the upload.
-    uploadTask.on(
-      firebase.storage.TaskEvent.STATE_CHANGED, // or 'state_changed'
-      function (snapshot) {
-        // Get task progress, including the number of bytes uploaded and the total number of bytes to be uploaded
-        var progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
-        console.log('Upload is ' + progress + '% done');
-        switch (snapshot.state) {
-          case firebase.storage.TaskState.PAUSED: // or 'paused'
-            console.log('Upload is paused');
-            break;
-          case firebase.storage.TaskState.RUNNING: // or 'running'
-            console.log('Upload is running');
-            break;
-        }
-      },
-      function (error) {
-        switch (error.code) {
-          case 'storage/unauthorized':
-            // User doesn't have permission to access the object
-            break;
-          case 'storage/canceled':
-            // User canceled the upload
-            break;
-          case 'storage/unknown':
-            // Unknown error occurred, inspect error.serverResponse
-            break;
-        }
-      },
-      function () {
-        // Upload completed successfully, now we can get the download URL
-        uploadTask.snapshot.ref.getDownloadURL().then(function (downloadURL) {
-          console.log('File available at', downloadURL);
-          user
-            .updateProfile({
-              photoURL: downloadURL,
-            })
-            .then(function () {
-              console.log('photo updated');
-            })
-            .catch(function (error) {
-              console.log(error.message);
-            });
-        });
-      },
-    );
-  };
+  //   // Listen for state changes, errors, and completion of the upload.
+  //   uploadTask.on(
+  //     firebase.storage.TaskEvent.STATE_CHANGED, // or 'state_changed'
+  //     function (snapshot) {
+  //       // Get task progress, including the number of bytes uploaded and the total number of bytes to be uploaded
+  //       var progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+  //       console.log('Upload is ' + progress + '% done');
+  //       switch (snapshot.state) {
+  //         case firebase.storage.TaskState.PAUSED: // or 'paused'
+  //           console.log('Upload is paused');
+  //           break;
+  //         case firebase.storage.TaskState.RUNNING: // or 'running'
+  //           console.log('Upload is running');
+  //           break;
+  //       }
+  //     },
+  //     function (error) {
+  //       switch (error.code) {
+  //         case 'storage/unauthorized':
+  //           // User doesn't have permission to access the object
+  //           break;
+  //         case 'storage/canceled':
+  //           // User canceled the upload
+  //           break;
+  //         case 'storage/unknown':
+  //           // Unknown error occurred, inspect error.serverResponse
+  //           break;
+  //       }
+  //     },
+  //     function () {
+  //       // Upload completed successfully, now we can get the download URL
+  //       uploadTask.snapshot.ref.getDownloadURL().then(function (downloadURL) {
+  //         console.log('File available at', downloadURL);
+  //         user
+  //           .updateProfile({
+  //             photoURL: downloadURL,
+  //           })
+  //           .then(function () {
+  //             console.log('photo updated');
+  //           })
+  //           .catch(function (error) {
+  //             console.log(error.message);
+  //           });
+  //       });
+  //     },
+  //   );
+  // };
 
   return (
     <View style={styles.container}>
       <View style={styles.mainContainer}>
-        <TouchableOpacity style={styles.photoView} onPress={handleChoosePhoto}>
+        <TouchableOpacity style={styles.photoView}>
           <View style={styles.avatar}>
             {photoUrl ? (
               <Image
@@ -130,7 +128,8 @@ const ProfileScreen = ({navigation}) => {
                   borderRadius: 150 / 2,
                 }}
                 source={{
-                  uri: photoUrl,
+                  uri:
+                    'https://firebasestorage.googleapis.com/v0/b/gofootball-84467.appspot.com/o/football_player-512.png?alt=media&token=a2777978-fca9-4d88-b46b-682fac670961',
                 }}
               />
             ) : photo ? (
@@ -146,7 +145,10 @@ const ProfileScreen = ({navigation}) => {
               />
             ) : (
               <Image
-                source={require('../assets/avatar.png')}
+                source={{
+                  uri:
+                    'https://firebasestorage.googleapis.com/v0/b/gofootball-84467.appspot.com/o/football_player-512.png?alt=media&token=a2777978-fca9-4d88-b46b-682fac670961',
+                }}
                 style={{width: 100, height: 100}}
               />
             )}
